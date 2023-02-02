41eefc531a53d339959f15ce158b6dfeb2fbfaea
CatsArmy <104517593+CatsArmy@users.noreply.github.com>
2023-02-02 20:48:28 +0200

    Add project files.

---
+++ Classes WIth Arrays/Class1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_WIth_Arrays
+{
+    public class Class1
+    {
+        public static void PrintArr(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+        public static double AvgArr(int[] a)
+        {
+            double sum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum += a[i];
+            }
+            return sum / a.Length;
+        }
+        public static int SumArr(int[] a)
+        {
+            int sum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum += a[i];
+            }
+            return sum;
+        }
+        public static int MulArr(int[] a)
+        {
+            int mul = 1;
+            for (int i = 0; i < a.Length; i++)
+            {
+                mul *= a[i];
+            }
+            return mul;
+        }
+        public static void Odd2EvenArr(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] % 2 != 0)
+                {
+                    a[i] = a[i] * 2;
+                }
+            }
+        }
+        public static void DivNumArr(int[] a, int num)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] % num == 0)
+                {
+                    Console.WriteLine(a[i]);
+                }
+            }
+        }
+        public static int IndexMax(int[] a)
+        {
+            int maxIndex = 0;
+            int maxValue = a[0];
+
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[i] > maxValue)
+                {
+                    maxValue = a[i];
+                    maxIndex = i;
+                }
+            }
+            return maxIndex;
+        }
+        public static int GetMin(int[] a)
+        {
+            int min = a[0];
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[i] < min)
+                {
+                    min = a[i];
+                }
+            }
+            return min;
+        }
+        public static bool IsAscendingOrder(int[] a)
+        {
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                if (a[i] >= a[i + 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        public static int ContainNumberCounter(int[] a, int value)
+        {
+            int count = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == value)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public static bool ContainsNumber(int[] a, int value)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == value)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        public static int SumAppearanceCounter(int[] a, int value)
+        {
+            int count = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == value)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Classes WIth Arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using Classes_WIth_Arrays;
+
+Console.WriteLine("Enter the number of MaxIndex of the array");
+Console.WriteLine("Needed so you can input the values for the array");
+bool StopInputing=false;
+string pause;
+int n = int.Parse(Console.ReadLine());
+int[] arr = new int[n];
+
+for (int i = 0; i < n && !StopInputing; i++)
+{
+    Console.WriteLine($"Enter the value for the index: {i} of the array:");
+    arr[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Continue? enter anything then press enter");
+pause  = Console.ReadLine();
+Console.Clear();
+Console.WriteLine("Original array: ");
+Class1.PrintArr(arr);
+Console.WriteLine("");
+Console.WriteLine("Sum of array: " + Class1.SumArr(arr));
+Console.WriteLine("Average of array: " + Class1.AvgArr(arr));
+Console.WriteLine("Multiplication of array: " + Class1.MulArr(arr));
+Console.WriteLine("Max Index: " + Class1.IndexMax(arr));
+Console.WriteLine("Min Value: " + Class1.GetMin(arr));
+Console.WriteLine("Is Ascending Order: " + Class1.IsAscendingOrder(arr));
+Console.WriteLine("Contains number 2: " + Class1.ContainsNumber(arr, 2));
+Console.WriteLine("Sum Appearance Counter: " + Class1.SumAppearanceCounter(arr, 2));
+Console.WriteLine("");
+Console.WriteLine("Odd to even array: ");
+Class1.Odd2EvenArr(arr);
+Class1.PrintArr(arr);
+Console.WriteLine("");
+Console.WriteLine("Divisible by 3: ");
+Class1.DivNumArr(arr, 3);
